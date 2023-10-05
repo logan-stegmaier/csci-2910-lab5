2ba5c913609fdd5009078aa1e350bb7cebcb2692
logan-stegmaier <114032249+logan-stegmaier@users.noreply.github.com>
2023-10-05 01:48:46 -0400

    beginning stages of code
    Began to work on API code. 
    Running into a few issues reading properties in json - planning to figure this out so that I can decode the base64 string of the skin data

---
+++ API/API/MinecraftData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API
+{
+    public class MinecraftData
+    {
+        public string id { get; set; } = string.Empty;
+        public string name { get; set; } = string.Empty;
+
+        public string[] properties { get; set; } 
+
+        public string value { get; set; } = string.Empty;
+
+        public MinecraftData() { }
+
+        public MinecraftData(string id, string name, string value)
+        {
+            this.name = name;
+            this.id = id;
+            this.value = value;
+            
+        }
+
+        public override string ToString()
+        {
+            string minecraftString = "";
+
+            minecraftString += $"ID: {id}\n";
+            minecraftString += $"Username: {name}\n";
+            minecraftString += $"Value: {value}\n";
+
+            return minecraftString;
+        }
+
+    }
+}
---
+++ API/API/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+
+namespace API
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await MinecraftCall(); 
+
+            
+        }
+
+        public static async Task MinecraftCall()
+        {
+
+            var client = new HttpClient();
+
+            HttpResponseMessage response = await client.GetAsync("https://sessionserver.mojang.com/session/minecraft/profile/e1d7ed8ca711430ca69a84da53fae680");
+
+            string json = await response.Content.ReadAsStringAsync();
+
+            var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+
+            MinecraftData me = JsonSerializer.Deserialize<MinecraftData>(json, options);
+
+            Console.WriteLine(Base64Decode(me.value) + "\n");
+            
+            Console.WriteLine(me + "\n");
+        }
+
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+
+    }
+}
